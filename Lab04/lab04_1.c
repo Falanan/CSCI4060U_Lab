bbb29e1c665609740cb09de946d4efe8818b36a9
Wenbo Zhang <90272076+Falanan@users.noreply.github.com>
2023-02-16 17:41:27 -0500

    lab04_v2

---
+++ Lab04/lab04_1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int fib(int n) {
+    if (n < 2) {
+        return n;
+    } else {
+        return fib(n-1) + fib(n-2);
+    }
+}
+
+int main() {
+    int n = 20;
+
+    for (int i = 0; i < n; i++) {
+        int result = fib(i);
+        printf("%d ", result);
+    }
+    printf("\n");
+
+    return 0;
+}
---
+++ Lab04/lab04_2.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <omp.h>
+
+
+#define NUM_THREADS 4
+
+int fib(int n) {
+    if (n < 2) {
+        return n;
+    } else {
+        int x, y;
+        #pragma omp task shared(x)
+        x = fib(n-1);
+        #pragma omp task shared(y)
+        y = fib(n-2);
+        #pragma omp taskwait
+        return x + y;
+    }
+}
+
+int main() {
+    int n = 20;
+
+    // Set number of threads
+    omp_set_num_threads(NUM_THREADS);
+
+    #pragma omp parallel
+    {
+        #pragma omp for ordered
+        for (int i = 0; i < n; i++) {
+            #pragma omp ordered
+            {
+                int result = fib(i);
+                printf("%d ", result);
+            }
+            
+        }
+        // printf("\n");
+    }
+    printf("\n");
+
+    return 0;
+}
